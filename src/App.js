91512f484c295223f512e33cce08d26a0e45c430
Kangbumkyu <g.kangbumkyu@gmail.com>
2020-12-21 23:49:46 +0700

    Install prop-types

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import PropTypes from "prop-types"
 
+const foods = ["Gogi", "bread", "Chocolate"]
+
+function Food({fav}) {
+  
+  return <h3>I like {fav}</h3>
+}
+
+Food.propTypes = {
+  fav: PropTypes.string.isRequired
+}
 
 function App() {
+  
   return (
     <div className="App">
-      
+      {foods.map(function(item, index) { 
+        return <Food key={index} fav={item} />
+      })
+      }
     </div>
   );
 }
